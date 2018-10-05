a17972f8beb033f7d18c61deab40a0dbd2441020
Ashok Vishwakarma <ashokvishwakarma@ashok.local>
2018-10-05 14:36:47 +0530

    Added RedisStore

---
+++ src/RedisStore.js
@@ -0,0 +1,104 @@
+/**
+ * RedisStore
+ * 
+ * RedisStore for koa2-ratelimit
+ * 
+ * @author Ashok Vishwakarma <akvlko@gmail.com>
+ */
+
+/**
+ * Store
+ * 
+ * Existing Store class
+ */
+const Store = require('./Store.js');
+
+/**
+ * redis
+ * 
+ * promise-redis module
+ * https://github.com/maxbrieiev/promise-redis#readme
+ */
+const redis = require('promise-redis')();
+
+/**
+ * RedisStore
+ * 
+ * Class RedisStore
+ */
+class RedisStore extends Store {
+  /**
+   * constructor
+   * @param {*} config 
+   * 
+   * config is redis config
+   */
+  constructor(config){
+    super();
+    this.client = redis.createClient(config);
+
+    return {
+      incr: this.incr.bind(this),
+      decrement: this.decrement.bind(this),
+      saveAbuse: this.saveAbuse.bind(this)
+    }
+  }
+
+  /**
+   * _hit
+   * @access private
+   * @param {*} key 
+   * @param {*} options 
+   * @param {*} weight 
+   */
+  async _hit(key, options, weight) {
+
+    let [counter, dateEnd] = await this.client.multi().get(key).ttl(key).exec();
+    
+    if(counter === null) {
+      counter = weight;
+      dateEnd = Date.now() + options.interval;
+      await this.client.setex(key, dateEnd, counter);
+    }else {
+      counter = await this.client.incrby(key, weight);
+    }
+
+    return {
+      counter,
+      dateEnd
+    }
+  }
+
+  /**
+   * incr
+   * 
+   * Override incr method from Store class
+   * @param {*} key 
+   * @param {*} options 
+   * @param {*} weight 
+   */
+  async incr(key, options, weight) {
+    return await this._hit(key, options, weight);
+  }
+
+  /**
+   * decrement
+   * 
+   * Override decrement method from Store class
+   * @param {*} key 
+   * @param {*} options 
+   * @param {*} weight 
+   */
+  async decrement(key, options, weight) {
+    await this.client.decrby(key, weight);
+  }
+
+  /**
+   * saveAbuse
+   * 
+   * Override saveAbuse method from Store class
+   */
+  saveAbuse() {}
+}
+
+module.exports = RedisStore;
