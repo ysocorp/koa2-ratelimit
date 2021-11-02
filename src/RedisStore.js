559436f89b1630da1547a0f7173058c189ddf280
kuselan <kuselan@transactgraph.com>
2021-11-02 16:49:10 +0800

    node-redis replace promise-redis

--- src/RedisStore.js
+++ src/RedisStore.js
@@ -1,62 +1,65 @@
 /**
  * RedisStore
- * 
+ *
  * RedisStore for koa2-ratelimit
- * 
+ *
  * @author Ashok Vishwakarma <akvlko@gmail.com>
  */
 
 /**
  * Store
- * 
+ *
  * Existing Store class
  */
 const Store = require('./Store.js');
 
 /**
  * redis
- * 
- * promise-redis module
- * https://github.com/maxbrieiev/promise-redis#readme
+ *
+ * node-redis module
  */
-const redis = require('promise-redis')();
+const redis = require('redis');
 
 /**
  * RedisStore
- * 
+ *
  * Class RedisStore
  */
 class RedisStore extends Store {
   /**
    * constructor
-   * @param {*} config 
-   * 
+   * @param {*} config
+   *
    * config is redis config
    */
   constructor(config){
     super();
     this.client = redis.createClient(config);
+    this.client.on('error', (err) => console.log('Redis Client Error', err));
+    this.client.connect()
+
+
   }
 
   /**
    * _hit
    * @access private
-   * @param {*} key 
-   * @param {*} options 
-   * @param {*} weight 
+   * @param {*} key
+   * @param {*} options
+   * @param {*} weight
    */
   async _hit(key, options, weight) {
 
     let [counter, dateEnd] = await this.client.multi().get(key).ttl(key).exec();
-    
+
     if(counter === null) {
       counter = weight;
       dateEnd = Date.now() + options.interval;
 
       const seconds = Math.ceil(options.interval / 1000);
-      await this.client.setex(key, seconds, counter);
+      await this.client.setEx(key, seconds.toString(), counter.toString());
     }else {
-      counter = await this.client.incrby(key, weight);
+      counter = await this.client.incrBy(key, weight);
     }
 
     return {
@@ -67,11 +70,11 @@ class RedisStore extends Store {
 
   /**
    * incr
-   * 
+   *
    * Override incr method from Store class
-   * @param {*} key 
-   * @param {*} options 
-   * @param {*} weight 
+   * @param {*} key
+   * @param {*} options
+   * @param {*} weight
    */
   async incr(key, options, weight) {
     return await this._hit(key, options, weight);
@@ -79,19 +82,19 @@ class RedisStore extends Store {
 
   /**
    * decrement
-   * 
+   *
    * Override decrement method from Store class
-   * @param {*} key 
-   * @param {*} options 
-   * @param {*} weight 
+   * @param {*} key
+   * @param {*} options
+   * @param {*} weight
    */
   async decrement(key, options, weight) {
-    await this.client.decrby(key, weight);
+    await this.client.decrBy(key, weight);
   }
 
   /**
    * saveAbuse
-   * 
+   *
    * Override saveAbuse method from Store class
    */
   saveAbuse() {}
